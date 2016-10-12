89dc9c485c7b925ecdd4ad3d194b8a8cc17282f8
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-10-12 11:57:01 -0400

    Created new project, deleted GameScene.sks

---
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  SpriteKitSimpleGame
+//
+//  Created by Amy Joscelyn on 10/12/16.
+//  Copyright © 2016 Amy Joscelyn. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+
+    override var prefersStatusBarHidden: Bool
+    {
+        return true
+    }
+}
